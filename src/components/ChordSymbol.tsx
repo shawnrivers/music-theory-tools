532ef22e9eb729b04c00a9925c59a4940f58cb46
Shawn Rivers <kaushou@gmail.com>
2021-06-15 22:22:39 +0900

    Modify styles

--- src/components/ChordSymbol.tsx
+++ src/components/ChordSymbol.tsx
@@ -7,9 +7,9 @@ export const ChordSymbol: React.VFC<{
 }> = ({ symbol, className }) => (
   <span className={classNames('leading-none', className)}>
     <span>{symbol.base}</span>
-    {symbol.aside && <span>{symbol.aside}</span>}
+    {symbol.aside && <span className="text-sm">{symbol.aside}</span>}
     {symbol.supperScript && (
-      <span className="align-superscript">{symbol.supperScript}</span>
+      <span className="align-superscript text-sm">{symbol.supperScript}</span>
     )}
   </span>
 );
